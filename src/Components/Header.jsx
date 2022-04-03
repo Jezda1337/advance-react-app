3051162274de4a68b283a7e3532a73f61d5ed177
mihajlo-cirikovacki <cirikovackimihajlo@yahoo.com>
2022-04-04 00:32:30 +0200

    testing

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,38 +1,38 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 // Style
-import "../Style/Components/Header.scss";
+import '../Style/Components/Header.scss';
 
 const Header = ({ setPath, path }) => {
   return (
-    <header className="header">
-      <div className="wrapper">
-        <nav className="header__nav">
-          <ul className="header__list">
-            <li className="header__item">
+    <header className='header'>
+      <div className='wrapper'>
+        <nav className='header__nav'>
+          <ul className='header__list'>
+            <li className='header__item'>
               <Link
-                className="header__link header__link--animal"
-                style={path === "animal" ? { color: "#2a9d8f" } : null}
-                to="/animal"
+                className='header__link header__link--animal'
+                style={path === 'animal' ? { color: '#2a9d8f' } : null}
+                to='/animal'
                 onClick={(e) => setPath(e.target.innerHTML)}
               >
-                animal
+                animal dskljhgldfshkljghdfklghkl
               </Link>
             </li>
-            <li className="header__item">
+            <li className='header__item'>
               <Link
-                className="header__link header__link--dev"
-                style={path === "dev" ? { color: "#EB5E28" } : null}
-                to="/dev"
+                className='header__link header__link--dev'
+                style={path === 'dev' ? { color: '#EB5E28' } : null}
+                to='/dev'
                 onClick={(e) => setPath(e.target.innerHTML)}
               >
                 dev
               </Link>
             </li>
-            <li className="header__item">
+            <li className='header__item'>
               <Link
-                className="header__link header__link--history"
-                style={path === "history" ? { color: "#29ABE2" } : null}
-                to="/history"
+                className='header__link header__link--history'
+                style={path === 'history' ? { color: '#29ABE2' } : null}
+                to='/history'
                 onClick={(e) => setPath(e.target.innerHTML)}
               >
                 history
